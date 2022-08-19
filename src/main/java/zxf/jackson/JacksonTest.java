28ac27ea6aa0d726b1269e746ae9931dd227eb06
davis <zanxiaofeng@163.com>
2022-08-19 12:34:14 +0800

    update

---
+++ src/main/java/zxf/jackson/JacksonTest.java
@@ -0,0 +1,4 @@
+package zxf.jackson;
+
+public class JacksonTest {
+}
---
+++ src/main/java/zxf/jackson/model/MyAuthentication.java
@@ -0,0 +1,99 @@
+package zxf.jackson.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+public class MyAuthentication implements Authentication {
+    private MyUser myUser;
+
+    //Add for json serialize
+    public MyAuthentication(){
+    }
+
+    public MyAuthentication(MyUser myUser) {
+        this.myUser = myUser;
+    }
+
+    @Override
+    @JsonIgnore
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public Object getPrincipal() {
+        return myUser;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAuthenticated() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    @JsonIgnore
+    public String getName() {
+        return myUser.getName();
+    }
+
+    public MyUser getMyUser() {
+        return myUser;
+    }
+
+    public void setMyUser(MyUser myUser) {
+        this.myUser = myUser;
+    }
+
+    public static class MyUser implements Serializable {
+        private String name;
+        private Integer age = 1;
+
+        //Add for json serialize
+        public MyUser(){
+        }
+
+        public MyUser(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+    }
+}
